417e4c67ff81e4eef53fb080783fafb3cc2205af
Hamelin92 <holy_killer@naver.com>
2022-07-27 04:05:09 +0900

    0727

---
+++ code_/boj/partialsum/bj2143_two_array_sum.py
@@ -0,0 +1,10 @@
+from itertools import combinations
+
+T = int(input())
+n = int(input())
+A = list(map(int, input().split()))
+m = int(input())
+B = list(map(int, input().split()))
+A.sort()
+B.sort()
+
---
+++ code_/boj/topological_sort/bj2252_sorting.py
@@ -0,0 +1,8 @@
+N, M = map(int, input().split())
+students = list(range(1, N+1))
+for s in range(M):
+	A, B = map(int, input().split())
+
+
+
+
