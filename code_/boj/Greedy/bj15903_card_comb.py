900807d2c903d76efad42d50d2fb648f8edd0d11
Hamelin92 <holy_killer@naver.com>
2022-08-02 09:41:39 +0900

    0802

---
+++ code_/boj/Greedy/bj15903_card_comb.py
@@ -0,0 +1,13 @@
+import heapq
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+heapq.heapify(a)
+for com in range(m):
+	a_1 = heapq.heappop(a)
+	a_2 = heapq.heappop(a)
+	na = a_1 + a_2
+	heapq.heappush(a, na)
+	heapq.heappush(a, na)
+print(sum(a))
